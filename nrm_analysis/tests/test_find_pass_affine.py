dd6ca49d5aa0db5f57f251ca5a9bda7410389ae0
anand0xff <anand0xff@gmail.com>
2018-10-19 14:16:37 -0400

    found rotation and scale

--- nrm_analysis/tests/test_find_pass_affine.py
+++ nrm_analysis/tests/test_find_pass_affine.py
@@ -34,9 +34,9 @@ holeshape='hex'
 oversample = 1
 n_image = 77
 
-rotd_true = 50
-rotd_search = (49.0, 49.5, 50, 50.5, 51.0)
-
+rotd_true = 50.5 # actual rotation used in the affine2d object when creating simulated data
+rotd_search = (49.5, 49.9, 50.4, 51.1, 51.5)
+scale_search = (0.975, 0.985, 0.995, 1.005, 1.015) # avoid the exact hit of your simulated data's pixel scale, i.e. 1.000
 
 class FindPassAffine2dTestCase(unittest.TestCase):
     def setUp(self):
@@ -48,7 +48,7 @@ class FindPassAffine2dTestCase(unittest.TestCase):
         monochromatic_wavelength_m = 4.3e-6 
         mask = 'MASK_NRM'
         filter = 'F430M'
-        pixel = 0.0656 
+        pixel = 0.0656  # arcsec
         filter_name = 'Monochromatic '+np.str(monochromatic_wavelength_m)
 
         self.filter = filter
@@ -62,7 +62,7 @@ class FindPassAffine2dTestCase(unittest.TestCase):
         self.datadir = datadir
 
         # file containing the test data
-        imagefn = datadir+"/imagedata.fits"
+        imagefn = datadir+"/simulated_image.fits"
         # use millidegrees and integers
         imagefn = imagefn.replace(".fits","_truerotmd{0:+05d}.fits".format(int(1000.0*float(rotd_true))))
         self.imagefn = imagefn
@@ -77,11 +77,12 @@ class FindPassAffine2dTestCase(unittest.TestCase):
         print('  >> >> >> >> set-up  self.savedir %s' % self.savedir)
 
 
-        # Create test data
+        # Create test data on disk
         
+        print("avoidhexsingularity:  ", utils.avoidhexsingularity(rotd_true))
         affine = utils.Affine2d(rotradccw=np.pi*utils.avoidhexsingularity(rotd_true)/180.0, 
                                 name="{0:.3}".format(float(rotd_true)))
-        affine.show(label="Creating PSF data")
+        affine.show(label="Creating PSF data with Affine2d")
         #
         from nrm_analysis.fringefitting.LG_Model import NRM_Model
         jw = NRM_Model(mask='jwst',holeshape="hex", affine2d=affine)
@@ -92,20 +93,18 @@ class FindPassAffine2dTestCase(unittest.TestCase):
         # PSF without oversampling
         fits.writeto(imagefn, jw.psf, overwrite=True)
         header = fits.getheader(imagefn)
-        header['PIXELSCL'] = pixel
+        header['PIXELSCL'] = (pixel, "arcseconds")
         header['FILTER'] = filter_name
         header['PUPIL'] = mask 
         header = utils.affinepars2header(header, affine)
         fits.update(imagefn, jw.psf, header=header)
-        self.imagedata = jw.psf.copy()
+        self.simulated_image = jw.psf.copy()
+        self.affine = affine # Affine2d parameters used to create test simulated_image ("true" value)
         del jw
         del affine
 
-        #else:
-        #    self.imagedata = fits.getdata(self.imagefn) 
-    
                 
-    def test_find_pass_affine(self):
+    def test_find_pass_affine_rot(self):
         
         header = fits.getheader(self.imagefn)   
         filename = os.path.basename(self.imagefn)
@@ -121,24 +120,75 @@ class FindPassAffine2dTestCase(unittest.TestCase):
         print("driver rotd_search:", rotd_search)
         print("driver rotd_true:", rotd_true)
         mx, my, sx,sy, xo,yo, = (1.0,1.0, 0.0,0.0, 0.0,0.0)
-        aff_best_rot =FAP.find_rotation(self.imagedata,
-                                        rotd_search, mx, my, sx,sy, xo,yo,
-                                        header['PIXELSCL']*arcsec2rad, 
-                                        n_image, self.monochromatic_wavelength_m, oversample, holeshape, 
-                                        outdir=None)
+
+        # FIND ROTATION FROM IMAGE DATA CREATED USING KNOWN ROTATION AND SCALE
+        aff_best_rot = FAP.find_rotation(self.simulated_image,
+                                         rotd_search, mx, my, sx,sy, xo,yo,
+                                         header['PIXELSCL']*arcsec2rad, 
+                                         n_image, self.monochromatic_wavelength_m, oversample, holeshape, 
+                                         outdir=None)
         print("rotd_measured:",  aff_best_rot)
         aff_best_rot.show(label="Measured ")
-
-
+        # you have to re-initialize an InstrumentData.NIRISS object with the 
+        # best rotation affine parameter now in order to use it in further data reduction
 
         if 'Monochromatic' in self.filter_name:
             nirissdata = InstrumentData.NIRISS(filt=self.filter, affine2d=aff_best_rot)
             nirissdata.wls = [self.monochromatic_wavelength_m]
             print((nirissdata.wls))
 
-        nirissdata.pscale_mas = header['PIXELSCL'] * 1000
-        nirissdata.pscale_rad = nirissdata.pscale_mas/1000. * arcsec2rad
-        #nirissdata.set_affine2d(aff_best_rot)
+        ff = nrm_core.FringeFitter(nirissdata, oversample=oversample,
+                      savedir=self.savedir, datadir=datadir,
+                      npix=n_image, interactive=False,
+                      hold_centering=True)
+        print(('FringeFitter oversampling: %d' % ff.oversample))
+    
+        threads = 1
+        ff.fit_fringes([filename])
+
+        CP_file = sorted(glob.glob(os.path.join(datadir,'%s/%s*.txt' % (filename.split('.')[0],'CPs_') )));
+        print("CP_file: ", type(CP_file), len(CP_file))
+        CP = Table.read(CP_file[0],format='ascii.no_header',names=({'closure_phase'}))
+
+        #         perform the actual test
+        self.assertTrue(np.mean(np.array(CP['closure_phase'])) < 1e-7, 
+            'Simulated closure phases of point source are non-zero')  # Avoiding hex singularity in hextransformEE.py causes this sort of noise in exactly-XYaligned PSF 2018 AS
+
+
+    def test_find_pass_affine_scl(self):
+        
+        header = fits.getheader(self.imagefn)   
+        filename = os.path.basename(self.imagefn)
+        datadir = os.path.dirname(self.imagefn)
+        print('\n')
+        print('  >> >> >> >> testing self.imagefn %s' % self.imagefn)
+        print('  >> >> >> >> testing self.datadir %s' % self.datadir)
+        print('  >> >> >> >> testing self.savedir %s' % self.savedir)
+
+        fn = filename.replace('.fits','')
+        print('  >> >> >> >> fn %s' % fn)
+
+        #mx, my, sx,sy, xo,yo, = (1.0,1.0, 0.0,0.0, 0.0,0.0)
+
+        # FIND SCALE FROM IMAGE DATA CREATED USING KNOWN ROTATION AND SCALE
+        # we use the known rotation for the affine2d
+        aff_best_scl = FAP.find_scale(   self.simulated_image,
+                                         self.affine, #  contains how the test image data was created
+                                         scale_search,
+                                         header['PIXELSCL']*arcsec2rad, 
+                                         n_image, self.monochromatic_wavelength_m, oversample, holeshape, 
+                                         outdir=None)
+        # you have to re-initialize an InstrumentData.NIRISS object with the
+        # best pscale_mas & pscale_rad parameter now in order to use it in further data reduction
+        aff_best_scl.show(label="aff_best_scl")
+
+        if 'Monochromatic' in self.filter_name:
+            nirissdata = InstrumentData.NIRISS(filt=self.filter, affine2d=aff_best_scl)
+            nirissdata.wls = [self.monochromatic_wavelength_m]
+            print((nirissdata.wls))
+        # naughty coding - adjust attributes to suit quick coding.  Just for testing...
+        nirissdata.pscale_mas = header['PIXELSCL'] * 1000  # set
+        nirissdata.pscale_rad = nirissdata.pscale_mas/1000. * arcsec2rad # propagate to convenience attribute
     
         ff = nrm_core.FringeFitter(nirissdata, oversample=oversample,
                       savedir=self.savedir, datadir=datadir,
